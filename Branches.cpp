a57491717f6ca8b51b69370b8e09f3b52d0a2f81
IT21225406 <99629832+IT21225406@users.noreply.github.com>
2022-05-20 15:25:36 +0000

    code by IT21225406

---
+++ Branches.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "Branches.h"
+using namespace std;
+
+branch::branch()
+{
+    branchID ="";
+    branchLocation="";
+    branchAddress="";
+    branchContact=0;
+}
+
+
+branch::~branch()
+{
+    
+}
