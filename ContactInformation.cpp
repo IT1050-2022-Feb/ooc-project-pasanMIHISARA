d87f016afccae589d53f59d15daca893b44c92cf
it21222986 <99384314+it21222986@users.noreply.github.com>
2022-05-20 15:35:31 +0000

    completed...

---
+++ ContactInformation.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include "ContactInformation.h"
+using namespace std;
+
+ContactInformation::ContactInformation()
+{
+	number = 0 ;
+	address = "" ;
+	email ="" ;
+	
+}
+
+ContactInformation::~ContactInformation()
+{
+	
+}
---
+++ Reports.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "Reports.h"
+
+using namespace std;
+
+
+
+Reports::Reports() {
+  Rid = "";
+  Rname = "";
+  RcreatedDate = "";
+  Rauthors = "";
+}
+
+Reports::~Reports() {
+
+	}
+
+
+
